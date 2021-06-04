7992636a309be2588d65f6fc9bcb2f90193c0d4a
mnuval21 <mnuval21@gmail.com>
2021-06-04 14:23:27 -0400

    looping-triangles

---
+++ looping_triangle.js
@@ -0,0 +1,16 @@
+//looping a triangle exercise
+//Write a loop that makes seven calls to console.log to output the following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+const x = '';
+for(let i = 0; i < *; i++){
+  x = '#' + x;
+  console.log(x);
+}
